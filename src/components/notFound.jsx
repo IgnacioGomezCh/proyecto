8790da6c3377c75c4dcf5384996117f93cb30806
jtoruno <41841500+jtoruno@users.noreply.github.com>
2019-11-05 23:17:21 -0600

    added 404 page

--- src/components/notFound.jsx
+++ src/components/notFound.jsx
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 import "./notFound.css";
 
+//https://www.fullstackreact.com/react-daily-ui/008-404-page/
 class NotFound extends Component {
+    state = {
+        image : 'http://i.giphy.com/l117HrgEinjIA.gif'
+    }
     render() {
-        return (<h1>NOT FOUND</h1>);
+        return (
+        <div className="FourOhFour">
+            <div className="bg" style={{ backgroundImage: 'url(' + this.state.image + ')'}}></div>
+            <div className="code">404</div>
+        </div>
+        );
     }
 }
 
